aed32b3db18863aa2ab00ba35ef3d8b08eb0103a
Ambrose McDuffy <ambrosemcduffy@gmail.com>
2022-04-10 23:32:52 -0400

    finished search algorithm

--- a_search.cpp
+++ a_search.cpp
@@ -13,37 +13,8 @@ using std::istringstream;
 using std::abs;
 using std::sort;
 
-enum class State {kEmpty, kObstacle, kClosed, kPath};
-
-
-int Heuristic(int x1, int x2, int y1, int y2){
-    /*
-     Manhattan Distance Heuristics function
-     */
-    int distance = abs((x2-x1) + (y2-y1));
-    return distance;
-}
-
-bool CheckValidCell(int x, int y, vector<vector<State>> &board){
-    bool on_board_x = (x >= 0 && x < board.size());
-    bool on_board_y = (y >= 0 && y < board[0].size());
-
-    if (on_board_x && on_board_y){
-        return board[x][y] == State::kEmpty;
-    }
-    return false;
-}
-
-bool Compare(const vector<int> node1, const vector<int> node2){
-    int f1 = node1[2] + node1[3];
-    int f2 = node2[2] + node2[3];
-    return f1 > f2;
-}
-
-
-void CellSort(vector<vector<int>> *v){
-    sort(v->begin(), v->end(), Compare);
-}
+enum class State {kEmpty, kObstacle, kClosed, kPath, kStart, kFinish};
+const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
 
 vector<State> ParseLine(string word){
@@ -96,6 +67,36 @@ vector<vector<State>> readBoardFile(){
 }
 
 
+bool Compare(const vector<int> node1, const vector<int> node2){
+    int f1 = node1[2] + node1[3];
+    int f2 = node2[2] + node2[3];
+    return f1 > f2;
+}
+
+
+void CellSort(vector<vector<int>> *v){
+    sort(v->begin(), v->end(), Compare);
+}
+
+
+int Heuristic(int x1, int x2, int y1, int y2){
+    /*
+     Manhattan Distance Heuristics function
+     */
+    return abs(x2-x1) + abs(y2-y1);
+}
+
+
+bool CheckValidCell(int x, int y, vector<vector<State>> &board){
+    bool on_board_x = (x >= 0 && x < board.size());
+    bool on_board_y = (y >= 0 && y < board[0].size());
+
+    if (on_board_x && on_board_y)
+        return board[x][y] == State::kEmpty;
+    return false;
+}
+
+
 void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &open_node, vector<vector<State>> &board){
     vector<int> node = {x, y, g, h};
     open_node.push_back(node);
@@ -103,30 +104,52 @@ void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &open_node, vecto
 }
 
 
+void ExpandNeighbors(const vector<int> &current, vector<vector<int>> &open, vector<vector<State>> &board, int goal[2]){
+    int x = current[0];
+    int y = current[1];
+    int g = current[2];
+
+    for (int i=0; i < 4; i ++){
+        int x2 = x + delta[i][0];
+        int y2 = y + delta[i][1];
+
+        if (CheckValidCell(x2, y2, board)){
+            int g2 = g+1;
+            int h2 = Heuristic(x2, y2, goal[0], goal[1]);
+            AddToOpen(x2, y2, g2, h2, open, board);
+        }
+    }
+}
+
+
 vector<vector<State>> Search(vector<vector<State>> board, int start[2], int goal[2]){
     vector<vector<int>> open = {};
+    
     int x = start[0];
     int y = start[1];
-
     int g = 0;
     int h = Heuristic(x, y, goal[0], goal[1]);
-
+    
     AddToOpen(x, y, g, h, open, board);
 
     while (open.size() > 0){
         CellSort(&open);
         auto current = open.back();
+        open.pop_back();
+
         x = current[0];
         y = current[1];
-
         board[x][y] = State::kPath;
+        
         if (x == goal[0] && y == goal[1]){
+            board[start[0]][start[1]] = State::kStart;
+            board[goal[0]][goal[1]] = State::kFinish;
             return board;
         } 
-
+        ExpandNeighbors(current, open, board, goal);
     }
     cout << "No Path Found!";
-    return vector<vector<State>> {};
+    return std::vector<vector<State>>{};
 }
 
 
@@ -137,6 +160,8 @@ string cellString(State cell){
     switch(cell){
         case State::kObstacle: return "⛰️  ";
         case State::kPath: return "🚗   ";
+        case State::kStart: return "🚦 ";
+        case State::kFinish: return "🏁 ";
         default: return "0 ";
     }
 }
@@ -157,11 +182,11 @@ void printBoard(vector<vector<State>> board){
 
 
 int main(){
-    auto board = readBoardFile();
-    int start[2] = {0, 0};
-    int goal[2] = {4, 5};
+    
+    int start[2]{0, 0};
+    int goal[2]{4, 5};
 
+    auto board = readBoardFile();
     auto solution = Search(board, start, goal);
-    cout << start << "\n";
     printBoard(solution);
 }
